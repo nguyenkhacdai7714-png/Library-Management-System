16e8379df15028249b55851aeb9ca1d7f0281662
TKF-20052011 <giakhanh.fpt.ktpm@gmail.com>
2026-05-28 19:45:40 +0700

    Update Member.java

--- LibraryManagement/src/librarymanagement/membermanagement/Member.java
+++ LibraryManagement/src/librarymanagement/membermanagement/Member.java
@@ -1,6 +1,70 @@
 
 package librarymanagement.membermanagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
-    
+
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+    private List<String> readingHistory;
+
+    public Member() {
+        this.id = "";
+        this.name = "";
+        this.phone = "";
+        this.email = "";
+        this.readingHistory = new ArrayList<>();
+    }
+
+    public Member(String id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.readingHistory = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getReadingHistory() {
+        return readingHistory;
+    }
+
+    public void setReadingHistory(List<String> readingHistory) {
+        this.readingHistory = readingHistory;
+    }
 }
