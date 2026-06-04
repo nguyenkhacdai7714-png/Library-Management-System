175244739900782f176c48bcb5398ed1b8563526
TKF-20052011 <giakhanh.fpt.ktpm@gmail.com>
2026-06-04 13:44:36 +0700

    Update 2 MemberManager.java

--- LibraryManagement/src/librarymanagement/membermanagement/MemberManager.java
+++ LibraryManagement/src/librarymanagement/membermanagement/MemberManager.java
@@ -11,7 +11,7 @@ public class MemberManager {
 
     private MemberManager() {}
     
-    public static MemberManager GetInstance() {
+    public static MemberManager getInstance() {
         return instance;
     }
 
